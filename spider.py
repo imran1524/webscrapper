588720fe4369d0bfb62b99612d94a0e56f91e412
Imran Khan <imran@Imrans-MBP.home>
2017-11-12 01:43:15 +0000

    Added BeautifulSoup module

--- spider.py
+++ spider.py
@@ -1,8 +1,13 @@
 # Import Request module
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://www.hot-dinners.com/Features/Articles/new-and-recently-opened-london-restaurants'
 
 response = requests.get(url)
 data = response.text
 # print(data)
+
+soup = BeautifulSoup(data,'html.parser')
+links = soup.prettify()
+print(links)
